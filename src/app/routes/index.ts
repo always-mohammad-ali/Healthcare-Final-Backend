2d7d434f022240999828e1f46e93d73985b48946
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-06-28 18:47:31 +0600

    doctor creation done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { AuthRoute } from "../module/auth/auth.route";
+import { UserRoute } from "../module/user/user.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoute);
 
 router.use("/specialties", SpecialtyRoutes)
+
+router.use("/user", UserRoute)
 
 
 
