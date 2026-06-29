07de7a0069537dd3d82e2b54ba8b45e9ef431321
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-06-29 15:55:47 +0600

    handling zod error done

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,18 +2,54 @@ import { NextFunction, Request, Response } from "express";
 import { envVar } from "../config/env";
 import { success } from "better-auth";
 import status from "http-status";
+import z from "zod";
+import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
+import { handleZodError } from "../errorHelpers/handleZodError";
+
+
+
+//Zod Error Patttern  from zod website basic usage
+    /*
+     error.issues; 
+    /* [
+      {
+        expected: 'string',
+        code: 'invalid_type',
+        path: [ 'username' , 'password' ], => username password
+        message: 'Invalid input: expected string'
+      },
+      {
+        expected: 'number',
+        code: 'invalid_type',
+        path: [ 'xp' ],
+        message: 'Invalid input: expected number'
+      }
+    ] 
+    */
 
 export const globalErrorHandler = (err : any, req : Request, res : Response, next : NextFunction) =>{
     if(envVar.NODE_ENV === "development"){
         console.log("Error from global error handler", err);
     }
     
-    const statusCode : number = status.INTERNAL_SERVER_ERROR;
-    const message : string = "Internal Server Error"
-    
-    res.status(statusCode).json({
+    let errorSources : TErrorSources[] = [];
+
+    let statusCode : number = status.INTERNAL_SERVER_ERROR;
+    let message : string = "Internal Server Error"
+
+    if(err instanceof z.ZodError){
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number;
+        message = simplifiedError.message;
+        errorSources = [...simplifiedError.errorSources];
+    }
+
+    const errorResponse : TErrorResponse = {
         success : false,
         message : message,
-        error : err.message
-    });
+        errorSources,
+        error : envVar.NODE_ENV === 'development' ? err : undefined,
+    }
+    
+    res.status(statusCode).json(errorResponse);
 }
