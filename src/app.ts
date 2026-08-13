50381caec910bd4f3110a381054611be4bf6b690
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-08-13 09:25:33 +0600

    stripe webhook even handling service, controller and route done

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { auth } from "./app/lib/auth";
 import path from "path";
 import cors from "cors";
 import { envVar } from "./app/config/env";
+import { PaymentController } from "./app/module/payment/payment.controller";
 
 const app: Application = express();
 
@@ -15,10 +16,7 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve(process.cwd(), `src/app/templates`) );
 
 
-app.post("/webhook", express.raw({type : "application/json"}), async(req : Request, res : Response) =>{
-  console.log("webhook received : ", req.body);
-  res.status(200).json({received : true})
-})
+app.post("/webhook", express.raw({type : "application/json"}), PaymentController.handleStripeWebhookEvent);
 
 app.use(cors({
   origin : [envVar.BETTER_AUTH_URL, envVar.FRONTEND_URL, "http://localhost:3000", "http://localhost:5000"],
